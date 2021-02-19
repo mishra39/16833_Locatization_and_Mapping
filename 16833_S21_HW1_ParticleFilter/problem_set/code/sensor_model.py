db7926ad97ac63e379f8397485d247ad8c11ac63
mishra39 <akshitmishra1995@gmail.com>
2021-02-19 18:45:23 -0500

    Initiated Motion Model code

---
+++ 16833_S21_HW1_ParticleFilter/problem_set/code/sensor_model.py
@@ -0,0 +1,80 @@
+'''
+    Adapted from course 16831 (Statistical Techniques).
+    Initially written by Paloma Sodhi (psodhi@cs.cmu.edu), 2018
+    Updated by Wei Dong (weidong@andrew.cmu.edu), 2021
+'''
+
+import numpy as np
+import math
+import time
+from matplotlib import pyplot as plt
+from scipy.stats import norm
+
+from map_reader import MapReader
+
+
+class SensorModel:
+    """
+    References: Thrun, Sebastian, Wolfram Burgard, and Dieter Fox. Probabilistic robotics. MIT press, 2005.
+    [Chapter 6.3]
+    """
+    def __init__(self, occupancy_map):
+        """
+        TODO : Tune Sensor Model parameters here
+        The original numbers are for reference but HAVE TO be tuned.
+        """
+        self._z_hit = 1
+        self._z_short = 0.1
+        self._z_max = 0.1
+        self._z_rand = 100
+
+        self._sigma_hit = 50
+        self._lambda_short = 0.1
+
+        self._max_range = 1000
+        self._min_probability = 0.35
+        self._subsampling = 2
+
+        self._norm_wts = 1.0
+
+    def beam_range_finder_model(self, z_t1_arr, x_t1):
+        """
+        param[in] z_t1_arr : laser range readings [array of 180 values] at time t
+        param[in] x_t1 : particle state belief [x, y, theta] at time t [world_frame]
+        param[out] prob_zt1 : likelihood of a range scan zt1 at time t
+        """
+        prob_zt1 = 1.0
+        k_tot = z_t1_arr.shape[0]
+        print(k_tot)
+        for k in range(0,k_tot):
+            # compute z_star_k (true measurement) using ray casting
+            
+            print('Laser Reading: ' + str(z_t1_arr[k])) 
+
+            if ((z_t1_arr[k] >= 0) and (z_t1_arr[k] <= self._max_range)):
+                p_hit = np.random.normal(loc=z_star_k, scale=self._sigma_hit, size=None)
+            else:
+                p_hit = 0
+            
+            if ((z_t1_arr[k] >= 0) and (z_t1_arr[k] <= z_star_k)):
+                nu = 1 / (1 - math.exp(-self._lambda_short*z_star_k))
+                p_short = nu * self._lambda_short * math.exp(-self._lambda_short*z_t1_arr[k])
+            else:
+                p_short = 0
+            
+            if (z_t1_arr[k] == self._max_range):
+                p_max = 1
+            else:
+                p_max = 0
+            
+            if ((z_t1_arr[k] >= 0) and (z_t1_arr[k] < self._max_range)):
+                p_rand = 1 / self._max_range
+            else:
+                p_rand = 0
+            
+            p = self._z_hit*p_hit + self._z_short*p_short + self._z_max*p_max + self._z_rand*p_rand
+            prob_zt1 = p * prob_zt1
+
+        '''return prob_zt1'''
+        prob_zt1 = 1.0
+        return prob_zt1
