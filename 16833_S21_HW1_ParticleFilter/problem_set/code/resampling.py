9b6d722626e54f11fd7ca198fe49387b7efc3dc1
Awith133 <48361786+Awith133@users.noreply.github.com>
2021-02-19 21:18:29 -0500

    lowvariance implmented with test

--- 16833_S21_HW1_ParticleFilter/problem_set/code/resampling.py
+++ 16833_S21_HW1_ParticleFilter/problem_set/code/resampling.py
@@ -5,7 +5,7 @@
 '''
 
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class Resampling:
     """
@@ -38,4 +38,35 @@ class Resampling:
         """
         #hi this is initial commit
         X_bar_resampled =  np.zeros_like(X_bar)
+        M = len(X_bar[:,0])
+        r = np.random.randint(0,M-1)
+        c = X_bar[0,3]
+        i = 1
+        
+        for m in range(1,M):
+            U = r+ (m-1)*(M-1)
+            while U>c and i<M-1:
+                i = i+1
+                c = c+X_bar[i,3]
+            X_bar_resampled[m,:] = X_bar[i,:] 
         return X_bar_resampled
+
+def test():
+    #Resamples points 10 times
+    pose = np.random.uniform(0, 359, (100,3))
+    weights = np.random.uniform(0,100,(100,1))
+    # weights = weights/sum(weights)
+    x_bar = np.append(pose,weights,1)
+    sample = Resampling()
+    xs1 = x_bar[:,0]
+    ys1 = x_bar[:,1]
+    plt.scatter(xs1,ys1)
+    for _ in range(10):
+        xs = x_bar[:,0]
+        ys = x_bar[:,1]
+        plt.scatter(xs1,ys1,c="black")
+        plt.scatter(xs,ys,c="red")
+        plt.show()
+        x_bar = sample.low_variance_sampler(x_bar)
+
+test()
