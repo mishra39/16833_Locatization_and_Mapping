f1abf84e59f25a157a1d33fe8fadcaf8be2e50b8
Awith133 <48361786+Awith133@users.noreply.github.com>
2021-02-19 19:03:23 -0500

    initial commit on sampling branch

--- 16833_S21_HW1_ParticleFilter/problem_set/code/resampling.py
+++ 16833_S21_HW1_ParticleFilter/problem_set/code/resampling.py
@@ -36,5 +36,6 @@ class Resampling:
         """
         TODO : Add your code here
         """
+        #hi this is initial commit
         X_bar_resampled =  np.zeros_like(X_bar)
         return X_bar_resampled
