Corrected Low Variance Sampling

16833_S21_HW1_ParticleFilter/problem_set/code/main.py
@@ -8,6 +8,7 @@ import argparse
 import numpy as np
 import sys, os
 
+import multiprocessing
 from multiprocessing import Pool
 from map_reader import MapReader
 from motion_model import MotionModel
@@ -80,17 +81,16 @@ def init_particles_freespace(num_particles, occupancy_map):
         for i in range(num_particles):
             more_particles = True # indicates if more particles needed
             while more_particles:
-                idx = np.random.randint(0,3)  # choose a random region in freespace
 
-                if idx == 0: # long corridor region
-                    x_pos = np.random.randint(3700,4000)
-                    y_pos = np.random.randint(2400,7500)
+                if (i < num_particles/4): # long corridor region
+                    x_pos = np.random.randint(3800,4280)
+                    y_pos = np.random.randint(2300,7470)
                 
-                elif idx == 1: # room in the middle of the corridor
+                elif (i >= num_particles/4 and i < num_particles/2): # room in the middle of the corridor
                     x_pos = np.random.randint(4200,5000)
                     y_pos = np.random.randint(3700,4500)
                 
-                elif idx == 2: # bottom right region
+                elif (i >= num_particles/2 and i < 3*num_particles/4): # bottom right region
                     x_pos =np.random.randint(5800,6800)
                     y_pos = np.random.randint(0,2500)
                 
@@ -101,7 +101,7 @@ def init_particles_freespace(num_particles, occupancy_map):
                 map_x = (int)(x_pos/10)
                 map_y = (int)(y_pos/10)
                 
-                if (occupancy_map[map_x,map_y] < 0.35 and occupancy_map[map_x,map_y] >=0): # check if the initialized points are in freespace
+                if (occupancy_map[map_y, map_x] < 0.35 and occupancy_map[map_y, map_x] >=0): # check if the initialized points are in freespace
                     more_particles = False
                     print(occupancy_map[map_y,map_x])
                 
@@ -201,19 +201,22 @@ if __name__ == '__main__':
             """
             x_t0 = X_bar[m, 0:3]
             x_t1 = motion_model.update(u_t0, u_t1, x_t0)
+            
             ########## Only for Debugging Motion Model. Delte Afterwards#######################
-            #X_bar_new[m, :] = np.hstack((x_t1, X_bar[m, 3]))
+            X_bar_new[m, :] = np.hstack((x_t1, X_bar[m, 3]))
             ###################################################################################
             """
             SENSOR MODEL
             """
-            if (meas_type == "L"):
+            '''if (meas_type == "L"):
                 z_t = ranges
                 w_t = sensor_model.beam_range_finder_model(z_t, x_t1)
+                print("Updated weight: " + str(w_t))
                 X_bar_new[m, :] = np.hstack((x_t1, w_t))
 
             else:
-                X_bar_new[m, :] = np.hstack((x_t1, X_bar[m, 3]))
+                pass
+                X_bar_new[m, :] = np.hstack((x_t1, X_bar[m, 3]))'''
 
         X_bar = X_bar_new
         u_t0 = u_t1
@@ -221,7 +224,5 @@ if __name__ == '__main__':
         RESAMPLING
         """
         #X_bar = resampler.low_variance_sampler(X_bar)
-
         if args.visualize:
             visualize_timestep(X_bar, time_idx, args.output)
-            #visualize_timestep(X_test,time_idx, args.output)

16833_S21_HW1_ParticleFilter/problem_set/code/motion_model.py
@@ -35,8 +35,8 @@ class MotionModel:
         """
         TODO : Add your code here
         """
-        print("X_t0: ")
-        print(x_t0)
+        #print("X_t0: ")
+        #print(x_t0)
         x_est_t0, y_est_t0, theta_est_t0 = u_t0
         x_est_t1, y_est_t1, theta_est_t1 = u_t1
         x_prev, y_prev, theta_prev = x_t0
@@ -49,14 +49,14 @@ class MotionModel:
         std_rot1 = self._alpha1*del_rot1**2 + self._alpha2*del_trans**2
         std_trans = self._alpha3*del_trans**2 + self._alpha4*del_rot1**2 + self._alpha4*del_rot2**2
         std_rot2 = self._alpha1*del_rot2**2 + self._alpha2*del_trans**2
-        print("dl_trans: ")
+        print("dl_trans ")
         print(del_trans)
 
         del_rot1_hat = del_rot1 - np.random.normal(loc=0.0,scale=std_rot1, size=None)
         del_trans_hat = del_trans - np.random.normal(loc=0.0, scale=std_trans,size=None)
         del_rot2_hat = del_rot2 - np.random.normal(loc=0.0,scale=std_rot2,size=None)
-        print("dl_trans_hat: ")
-        print(del_trans_hat)
+        #print("dl_trans_hat: ")
+        #print(del_trans_hat)
 
         x_t1 = x_prev + del_trans_hat * math.cos(theta_prev + del_rot1_hat)
         y_t1 = y_prev + del_trans_hat * math.sin(theta_prev + del_rot1_hat)

16833_S21_HW1_ParticleFilter/problem_set/code/resampling.py
@@ -5,7 +5,7 @@
 '''
 
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class Resampling:
     """
@@ -36,5 +36,49 @@ class Resampling:
         """
         TODO : Add your code here
         """
-        X_bar_resampled =  np.zeros_like(X_bar)
+        #hi this is initial commit
+        X_bar_resampled =  []
+        wt_arr = X_bar[:,3]
+
+        # Normalized weights
+        wt_arr = wt_arr / np.sum(wt_arr)
+        M = len(X_bar[:,0])
+        
+        r = np.random.uniform(0,1/M)
+        c = wt_arr[0]
+        i = 0
+        
+        for m in range(0,M):
+            U = r + (m/M)
+            while U>c:
+                print("in while loop")
+                i = i+1
+                c += wt_arr[i]
+            X_bar_resampled.append(X_bar[i])
+        X_bar_resampled = np.asarray(X_bar_resampled)
         return X_bar_resampled
+
+def test():
+    #Resamples points 10 times
+    X_bar = np.array([[94.234001,250.953995,-1.342158,0.025863],[294.234001,139.953995,-1.342158,0.0079745],
+    	[194.234001,439.953995,1.342158,0.0013982],[594.234001,339.953995,1.342158,0.200218]])
+    print (X_bar)
+    print(X_bar[0,3])
+    xs1 = X_bar[:,0]
+    ys1 = X_bar[:,1]
+    for i in range(100):
+        plt.scatter(xs1,ys1,c="red")    
+        sampleObj = Resampling()
+        X_bar_resampled = sampleObj.low_variance_sampler(X_bar)
+        print (X_bar_resampled)
+        print("Iteration: " + str(i))
+        xs1 = X_bar[:,0]
+        ys1 = X_bar[:,1]
+        #print (xs1,ys1)
+        xs1 = X_bar_resampled[:,0]
+        ys1 = X_bar_resampled[:,1]
+        plt.axis([0, 800, 0, 800])
+
+    #plt.show()
+if __name__ == '__main__':
+    test()